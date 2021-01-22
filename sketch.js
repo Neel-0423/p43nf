50ad8b1fa9e4260f1a97f2b56584bf8be87bc777
Neel-0423 <71958781+Neel-0423@users.noreply.github.com>
2021-01-22 12:51:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var h = hour();
+text('Current hour:\n' + h, 5, 50);
+
+var m = minute();
+text('Current minute: \n' + m, 10, 50);
+
+var s = second();
+text('Current second: \n' + s, 15, 50);
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background("black");  
+  drawSprites();
+}
